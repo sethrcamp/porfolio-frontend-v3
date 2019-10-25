672dd86d8d9e4c2754279d57b8f3a88423f10f80
Seth Campbell <sethrcamp@gmail.com>
2019-10-25 19:14:22 -0400

    ?

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,6 @@
 export const DEVELOPMENT_MODE = true;
 
 export const LOCAL_BASE_URL = "http://localhost:8888/portfolio/portfolio-backend/api/";
-export const SERVER_BASE_URL = "";
+export const SERVER_BASE_URL = "https://sethrcamp.com/";
 
 export const API_BASE_URL = DEVELOPMENT_MODE ? LOCAL_BASE_URL : SERVER_BASE_URL;
